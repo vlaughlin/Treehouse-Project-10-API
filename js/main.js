85a435263abd35d8d965d829e81cd93fbbea63fc
vlaughlin <vrodriguez10@gmail.com>
2017-10-04 22:24:33 -0400

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $.ajax({
-    url: 'https://randomuser.me/api/?results=12',
+    url: 'https://randomuser.me/api?results=12& inc = name, email, picture, location, cell, login, dob & nat = us',
     dataType: 'json',
     success: function (data) {
         console.log(data);
+
+
     }
+
+    document.getElementById('image').src = data.results[0].picture.large;
+    document.getElementById('firstName').textContent = data.results[0].name.first;
+    document.getElementById('lastName').textContent = data.results[0].name.last;
+
+
 });
