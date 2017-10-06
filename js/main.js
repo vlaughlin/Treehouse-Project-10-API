2c11fd75796a642518ebccc4d120bc9cd5554b33
vlaughlin <vrodriguez10@gmail.com>
2017-10-05 22:54:08 -0400

    changed id name

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
 $.ajax({
-    url: 'http://api.randomuser.me/',
+    url: 'https://randomuser.me/api?results=12& inc = name, email, picture, location, cell, login, dob & nat = us',
     dataType: 'jsonp',
     success: function (data) {
         console.log(data);
         
         $('<img />', {
             src: data.results[0].picture.large
-        }).appendTo('#result')
+        }).appendTo('#employeelist')
     }
 });
 
